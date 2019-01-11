6e86d980cf1e4b3d17bd0ad2ade23778f15ed027
carrolji <jirakul@ualberta.ca>
2019-01-10 17:51:59 -0700

    python request script

---
+++ get_requests.py
@@ -0,0 +1,14 @@
+#!/ur/bin/env python
+
+import requests
+
+print(requests.__version__)
+
+r = requests.get("http://www.google.com")
+
+#print(r)
+#print(dir(r))
+print(r.text)
+print(r.status_code)
+# print(r.headers)
+# print(r.content)
