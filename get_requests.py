aba4df5f625f793590de62748a985b1be3579fc2
carrolji <jirakul@ualberta.ca>
2019-01-10 22:50:45 -0700

    modified url

--- get_requests.py
+++ get_requests.py
@@ -4,7 +4,7 @@ import requests
 
 print(requests.__version__)
 
-r = requests.get("http://www.google.com")
+r = requests.get("https://raw.githubusercontent.com/carrolji/cmput404/master/get_requests.py")
 
 #print(r)
 #print(dir(r))
